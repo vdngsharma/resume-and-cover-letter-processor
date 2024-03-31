fbd463d75cc47afa39072e35c994af641ff52224
vshar117 <vshar117@asu.edu>
2024-03-30 17:42:20 -0700

    Finalizing Document Processor

---
+++ backend/app/document_processor.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import dotenv_values
+from docx import Document
+from docx2pdf import convert
+
+class DocumentProcessor:
+
+    def __init__(self, data):
+        self.env_vars = dotenv_values('.env')
+        self.target_word = data.get('target_word')
+        self.replacement_word = data.get('replacement_word')
+        self.name = '_'.join(self.env_vars.get('NAME').split())
+        self.job_id = data.get('job_id')
+        type_ = data.get('type')
+
+        if type_ is 'NonTechnical': 
+            self.doc = Document(self.env_vars.get('NON_TECHNICAL_COVER_LETTER_PATH'))
+        else:
+            self.doc = Document(self.env_vars.get('TECHINCAL_COVER_LETTER_PATH'))
+        
+
+    def process_document(self):
+        for paragraph in self.doc.paragraphs:
+            if self.target_word in paragraph.text:
+                paragraph.text = paragraph.text.replace(self.target_word, self.replacement_word)
+
+            for run in paragraph.runs:
+                if self.target_word in run.text:
+                    run.text = run.text.replace(self.target_word, self.replacement_word)
+
+        self.save_document()
+        
+
+    def save_document(self):
+        directory_name = self.job_id
+        parent_directory_path = self.env_vars.get('SAVE_DIRECTORY_PATH')
+        save_directory_path = os.path.join(parent_directory_path, directory_name)
+        if not os.path.exists(save_directory_path):
+            os.makedirs(save_directory_path)
+
+        file_name = self.name + '_' + directory_name + '_Cover_Letter.docx'
+        file_save_path = os.path.join(save_directory_path, file_name)
+
+        self.doc.save(file_save_path)
+        convert(file_save_path)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,24 +1,18 @@
 from flask import Flask, request, jsonify
-from app.word_processor import WordProcessor
+from document_processor import DocumentProcessor
 
 app = Flask(__name__)
 
-@app.route('/process_cover_letter', method = ['POST'])
+@app.route('/process_document', methods = ['POST'])
 def process_word():
-    file_path = request.form.get() # TODO: file_path
-    target_word = request.form.get() # TODO: target_word
-    replacement_word = request.form.get() # TODO: replacement_word
-
-    if not file_path or not target_word or not replacement_word:
-        return jsonify({'error': 'Missing required parameters'}), 400
-    
     try:
-        word_processor = WordProcessor(file_path)
-        word_processor.find_replace(target_word, replacement_word)
-        word_processor.save_document() # TODO: Target_Path
+        data = request.json
+        word_processor = DocumentProcessor(data)
+        word_processor.process_document()
+
         return jsonify({'message': 'Cover Letter processed successfullly'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, port = 5001)
